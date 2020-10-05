98516ae0cf38a960b74577a399bf017d291fa5a1
DaniyalAkbar <daniyalakbar1217@gmail.com>
2020-10-05 18:40:23 +0500

    Module is completed

--- src/package_main/Main_20321273.java
+++ src/package_main/Main_20321273.java
@@ -18,6 +18,10 @@ public class Main_20321273 {
 	static ArrayList<String> expenses = new ArrayList<String>();
 	static ArrayList<String> properties = new ArrayList<String>();
 	static ArrayList<String> rents = new ArrayList<String>();
+	static ArrayList<String> summary = new ArrayList<String>();
+	static int count = 1;
+	static int prop_found = 0;
+	static String addr = "";
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
@@ -97,12 +101,12 @@ public class Main_20321273 {
 
 			case 1:
 				
-				ArrayList<String> summary = new ArrayList<String>();
-				ArrayList<String> result = new ArrayList<String>();	
+				ArrayList<String> result = new ArrayList<String>();
+				summary.clear();
 				Client_20321273 class_client = null;
 				System.out.println("\nPlease Enter a Property's Address to Search: ");
-				int count = 1;
-				int prop_found = 0;
+				count = 1;
+				prop_found = 0;
 				String addr = sc.next().trim();
 				for (String p : properties) {				// FETHCING ALL THE LIST OF PREPOERTY MATCHING THE ADDRESS FROM USER INPUT
 					if (p.toLowerCase().contains(addr.toLowerCase())) {
@@ -156,12 +160,11 @@ public class Main_20321273 {
 				
 				
 				//FETCHING THE NUMBER OF WEEKS OF RENT COLLECTED THROUGH USER INPUT
-				//int weeks = 0;
 				while(true) {
 					try {
 						System.out.println("\nHow many weeks of rent was collected for the chosen property?");
 						int weeks = sc.nextInt();
-						if(weeks<1 || weeks>7) { 
+						if(weeks<1) { 
 							System.out.println("Please Enter a Valid Week Number...");
 							continue;
 						}
@@ -183,13 +186,122 @@ public class Main_20321273 {
 
 				//PRINTING SUMMARY
 				System.out.println("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+				System.out.println("\t\tSUMMARY");
 				for (String s : summary) {				// DISPLAYING EACH ITEM IN SUMMARY ARRAYLIST
 					System.out.println(s);
 				}
 				System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
 				break;
 
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 			case 2:
+				
+				ArrayList<String> output = new ArrayList<String>();
+				summary.clear();
+				System.out.println("\nPlease Enter a Property's Address to Search: ");
+				count = 1;
+				prop_found = 0;
+				addr = sc.next().trim();
+				for (String p : properties) {				// FETHCING ALL THE LIST OF PREPOERTY MATCHING THE ADDRESS FROM USER INPUT
+					if (p.toLowerCase().contains(addr.toLowerCase())) {
+						output.add(count + ") @" + p);
+						count++;
+						prop_found = 1;
+					}
+				}
+				if(prop_found == 0) {
+					System.out.println("No Such Property Exist in Records\n\n\n");
+					break;
+				}
+				
+				
+				// DISPLAYING ALL THE SEARCHED PROPERTIES
+				result_prop(output);
+				
+				
+				System.out.println("\nPlease Select an Address: ");
+				float week_rent_amount = 0;
+				
+				while (true) {
+					try {
+						String prop_owner = "";
+						String property = output.get(sc.nextInt() - 1);	//FETCHING SELECTED ADDRESS THROUGH USER INPUT AND "-1" FOR GETTING THE INDEX OF THE ARRAY
+						String[] prop_array = property.split(",");
+						week_rent_amount = Float.parseFloat(prop_array[prop_array.length - 3]);	//FETCHING THE WEEKLY RENT AMOUNT OF THE SELECTED PROPERTY
+						for (String c : clients) {						//FETCHING THE PROPERTY OWNERS FULL NAME FROM THE ARRAYLIST
+							String[] client = c.split(",");
+							if (client[0].equals(prop_array[prop_array.length - 1])) {
+								prop_owner = client[1];
+							}
+						}
+
+						
+						class_client = new Client_20321273(property, prop_owner);	//DISPLAYING PROPERTY ADDRESS, WEEKLY RENT CHARGED AND OWNERS FULL NAME
+						summary.add("PROPERTY ADDRESS: "+class_client.getAdd());	//ADDING PROPERTY ADDRESS TO THE SUMMARY ARRAYLIST
+						summary.add("OWNER'S NAME: "+class_client.getFullName());	//ADDING PROPERTY OWNER'S FULL NAME TO THE SUMMARY ARRAYLIST
+						
+						break;
+					} catch (IndexOutOfBoundsException e) {
+						System.out.println("Please Select a Valid Address");
+						continue;
+					}catch(InputMismatchException i) {
+						System.out.println("Can Only Accept Integers!");
+						continue;
+					}
+				}
+				
+				
+				System.out.println("\nWould You like to Record an Expense? Press 'Y' for yes and 'N' for no: ");
+				String ch = sc.next();
+				if(!ch.equals("Y") && !ch.equals("y")) { break; } 		//ANYTHING BESIDE 'Y' or 'y' IF CHOSEN, WOULD BE CONSIDERED A NO
+				
+				while(true) {
+					try {
+						System.out.println("\nPlease Enter an Amount");
+						float amt = sc.nextFloat();
+						if(amt<0) {									//VALIDATING IF THE AMOUNT IS NOT LESS THAN 0
+							System.out.println("The Amount Should be Greater Than 0. Please Enter Again");
+							continue;
+						}
+						String desc = desc();						//FETCHING DESCRIPTION FROM A FUNCTION + VALIDATION
+						
+						DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");  
+						LocalDateTime now = LocalDateTime.now();  
+						
+						summary.add("EXPENSE AMOUNT: "+amt);	//ADDING EXPENSE AMOUNT TO THE SUMMARY ARRAYLIST
+						summary.add("EXPENSE DESCRIPTION: "+desc);	//ADDING EXPENSE DESCRIPTION TO THE SUMMARY ARRAYLIST
+						summary.add("LAST EXPENSE ADDED: "+dtf.format(now));	//ADDING TODAYS DATE TO THE SUMMARY ARRAYLIST
+						
+						break;
+					}catch(InputMismatchException i) {
+						System.out.println("Can Only Accept Integers!");
+						continue;
+					}catch (Exception e) {
+						System.out.println("Please Enter an Amount in Numbers Only");
+						continue;
+					}
+				}
+
+				
+				
+				//PRINTING SUMMARY
+				System.out.println("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+				System.out.println("\t\tSUMMARY");
+				for (String s : summary) {				// DISPLAYING EACH ITEM IN SUMMARY ARRAYLIST
+					System.out.println(s);
+				}
+				System.out.println("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n");
+				
 				break;
 			case 3:
 				break;
@@ -223,6 +335,12 @@ public class Main_20321273 {
 		return al;
 	}
 
+	
+	
+	
+	
+	
+	
 	// FETCHING THE ADDRESS FROM THE CLIENTS TEXT FILE TO DISPLAY TO USER
 	public static void result_prop(ArrayList<String> result) {
 		if (!result.isEmpty()) {
@@ -236,5 +354,34 @@ public class Main_20321273 {
 			System.out.println("No Records Found!");
 		}
 	}
+	
+	
+	
+	//FETCHING EXPENSE DESCRIPTION + VALIDATION
+	public static String desc() {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("\nPlease Enter a Description of the Expense");
+			String desc = sc.next();
+			if(desc.isEmpty() || desc.equals("") || desc.equals(null) || desc.length() < 1 ) {
+				System.out.println("Description is not Appropriate");
+				continue;
+			}
+			return desc;
+		}
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
